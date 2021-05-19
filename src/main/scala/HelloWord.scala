ca098a965bb274437b2feeaa06c8e0fd63dd33ec
@marie-kore <estherkore75@gmail.com>
2021-05-19 19:00:02 +0000

    Initial commit

---
+++ src/main/scala/HelloWord.scala
@@ -0,0 +1,37 @@
+object HelloWord {
+ def main(args:Array[String]): Unit = {
+  val text : String = "hello word, mon premier programme"
+   println(text)
+
+
+  def Macollection(): Unit = {
+   val Maliste = List("jde","tar","jar")
+   val Maliste2 :List[String] = List("jule","Richard")
+   Maliste.foreach(l => println(l.toUpperCase))
+  }
+  val Sequence = Seq("doh","rez","rime","faha","solo","kore","marie","ange","narcisse","liam")
+  val  Newseq : Seq[Int] = Sequence.map(i =>i.length)
+  val affich = Sequence.foreach(println )
+  val affich1 = Newseq.foreach (println)
+
+  /* flitre*/
+   val tri : Seq[String] = Sequence.filter(_.length() > 3 )
+   tri.foreach(println)
+  /*tuple*/
+   val monMap: Map[String,String] = Map (
+    "yop" -> "yopougon",
+    "ry" -> "riverra",
+    "ab" -> "abobo"
+  )
+
+  val l = monMap.values.map(l => l).toList
+  println(l)
+
+ }
+
+
+
+
+
+}
+
